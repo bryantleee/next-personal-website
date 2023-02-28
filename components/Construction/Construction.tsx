3711a7cdc563a50dd95c0341b4249969a0144221
Bryant Lee <bl692@cornell.edu>
2023-02-28 00:38:23 -0500

    Update font style and size

--- components/Construction/Construction.tsx
+++ components/Construction/Construction.tsx
@@ -1,10 +1,15 @@
 import styles from './Construction.module.scss'
+import { Open_Sans, Nunito } from 'next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
+
 
 const Construction = () => {
     return (
         <div className={styles.construction}>
-            <h2 className={styles.construction__text}> Under Construction </h2>
-            <h3 className={styles.construction__text}> Please come back again later! </h3>
+            <h2 className={`${styles.construction__text} ${nunito.className}`}> Under Construction </h2>
+            <h3 className={`${styles.construction__text} ${nunito.className}`}> Please come back again later! </h3>
         </div>
     )
 }
