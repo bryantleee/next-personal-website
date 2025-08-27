52fd75be0695bf2bdc6deaf7680ec2fa70a9bd3b
Bryant Lee <bryantleeat@gmail.com>
2025-08-26 23:14:08 -0400

    Upgrade yarn, minor website updates

--- components/Construction/Construction.tsx
+++ components/Construction/Construction.tsx
@@ -1,19 +1,33 @@
-import Link from 'next/image'
-import styles from './Construction.module.scss'
-import { Open_Sans, Nunito } from 'next/font/google'
+import styles from "./Construction.module.scss";
+import { Open_Sans, Nunito } from "next/font/google";
 
-const openSans = Open_Sans({ subsets: ['latin'] })
-const nunito = Nunito({ subsets: ['latin'] })
-
+const openSans = Open_Sans({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 const Construction = () => {
-    return (
-        <div className={styles.construction}>
-            <h2 className={`${styles.construction__text} ${nunito.className}`}> Hello! </h2>
-            <h3 className={`${styles.construction__text} ${nunito.className}`}> My name is Bryant Lee. I am a software engineer living in NYC.</h3>
-            <h3 className={`${styles.construction__text} ${nunito.className}`}> Please feel free to reach out or check out some of my work <a className={`${styles.resume}`} href="/resume.pdf" download="resume">here</a> or with the links below!</h3>
-        </div>
-    )
-}
+  return (
+    <div className={styles.construction}>
+      <h2 className={`${styles.construction__text} ${nunito.className}`}>
+        {" "}
+        Hello!{" "}
+      </h2>
+      <h3 className={`${styles.construction__text} ${nunito.className}`}>
+        {" "}
+        My name is Bryant Lee. I am a software engineer living in NYC, currently
+        working at Meta.
+      </h3>
+      <h3 className={`${styles.construction__text} ${nunito.className}`}>
+        {" "}
+        Outside of work, I love homelabbing, playing video games, and traveling!
+        I am currently trying to get better at photography.
+      </h3>
+      <h3 className={`${styles.construction__text} ${nunito.className}`}>
+        {" "}
+        Please feel free to reach out or find me on other platforms at the links
+        below!
+      </h3>
+    </div>
+  );
+};
 
-export default Construction
+export default Construction;
--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -1,17 +1,22 @@
-import Image from 'next/image'
-import styles from './Introduction.module.scss'
+import Image from "next/image";
+import styles from "./Introduction.module.scss";
 
 const Introduction = () => {
-    return (
-        <div className={styles.homepage_content}>
-        <p>
-          Hello! My name is Bryant Lee. 
-          I graduated from Cornell University in 2021 and currently a software engineer at Amazon in NYC!
-          Feel free to reach out to me at leezbrya[at]gmail.com
-        </p>
-        <Image src="/github-logo.png" alt="GitHub logo" width={300} height={300} />
-      </div>
-    )
-}
+  return (
+    <div className={styles.homepage_content}>
+      <p>
+        Hello! My name is Bryant Lee. I graduated from Cornell University in
+        2021 and currently a software engineer at Amazon in NYC! Feel free to
+        reach out to me at leezbrya[at]gmail.com
+      </p>
+      <Image
+        src="/github-logo.png"
+        alt="GitHub logo"
+        width={300}
+        height={300}
+      />
+    </div>
+  );
+};
 
-export default Introduction
+export default Introduction;
